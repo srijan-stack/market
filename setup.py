b6de1fdd73a6d20ba41903b9231db6489efd19d5
aSrijan <adhyayansrijan1998@gmail.com>
2022-12-20 17:24:43 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+from setuptools import setup
+setup(name="market",
+      version="0.1",
+      url='https://github.com/srijan-stack/market',
+      description="this is a market calculator",
+      long_description="use this to market",
+      author="srijan",
+      packages=["src"],
+      install_requires=[])
---
+++ src/prices.py
@@ -0,0 +1,5 @@
+def name(str):
+    return str
+
+def price(num):
+    return num
